5f5a90c669d8afe0d3accd9ff360a4d2de625f12
maxi758 <maxi758@gmail.com>
2024-02-06 11:58:28 -0300

    add login reducer

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -28,6 +28,26 @@ export const register = createAsyncThunk(
   }
 );
 
+export const login = createAsyncThunk(
+  'auth/login',
+  async (userData, thunkAPI) => {
+    const url = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/usuarios/login`;
+    console.log(userData);
+    console.log(url);
+    try {
+      const response = await axios.post(url, userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -40,6 +60,15 @@ const authSlice = createSlice({
         state.rol = action.payload.rol;
       })
       .addCase(register.rejected, (state, action) => {
+        console.log(action.payload);
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.userId = action.payload.usuario._id;
+        state.token = action.payload.token;
+        state.rol = action.payload.usuario.rol;
+      })
+      .addCase(login.rejected, (state, action) => {
         console.log(action.payload);
       });
   },
